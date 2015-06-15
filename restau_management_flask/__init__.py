a52a01edbd275101c32dc33d8cc23e2ed210c47b
Yanze LIU <yanzeliu.france@gmail.com>
2015-06-15 21:37:21 +0800

    load menu and pictures

--- restau_management_flask/__init__.py
+++ restau_management_flask/__init__.py
@@ -1,4 +1,9 @@
 from flask import Flask
+import os # operating system
+
 app = Flask(__name__)
 app.config.from_object('config')
+APP_ROOT = os.path.dirname(os.path.abspath(__file__)) # to get the root of the web app(a string with app root folder)
+APP_STATIC = os.path.join(APP_ROOT, "static") # get the location of the static folder
+
 from restau_management_flask import views
--- restau_management_flask/views.py
+++ restau_management_flask/views.py
@@ -1,11 +1,27 @@
-from restau_management_flask import app
-from flask import render_template
+from restau_management_flask import app, APP_STATIC
+from flask import render_template, redirect, url_for
 from .models import Restaurant
+import os
+
 
 restau_name = app.config["RESTAU_NAME"]
 table_nb = app.config["TABLE_NB"]
 r = Restaurant(restau_name, table_nb)
+menu_file = os.path.join(APP_STATIC, "menu.txt")
+r.load_menu(menu_file=menu_file)
+print(len(r.menu.dishes))
+
 
 @app.route("/")
 def index():
   return render_template("index.html", r = r)
+
+@app.route("/menu")
+def menu():
+  return render_template("menu.html", menu=r.menu)
+
+@app.route("/refresh-menu")
+def refresh_menu():
+  r.load_menu(menu_file = menu_file)
+  return redirect(url_for("menu"))
+
